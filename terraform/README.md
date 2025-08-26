e4aa34e6ba64af21673124c050073ecfd354446d
Dmitry Suprunov <dsuprunov@gmail.com>
2025-08-26 14:45:20 +0200

    Updated Terraform configuration for a single VM

--- terraform/README.md
+++ terraform/README.md
@@ -5,32 +5,21 @@ wget https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.i
 
 qm create 9001 \
   --name ubuntu-24.04-cloud-template \
+  --machine q35 --ostype l26 \
   --memory 1024 --balloon 1024 \
   --cpu host --cores 1 --sockets 1 --numa 0 \
   --scsihw virtio-scsi-single \
   --scsi0 local-lvm:0,import-from=/root/noble-server-cloudimg-amd64.img,discard=on,iothread=1,ssd=1 \
+  --bios ovmf --efidisk0 local-lvm:1,efitype=4m,pre-enrolled-keys=1 \
   --boot order=scsi0 \
-  --net0 virtio,bridge=vmbr0 \
+  --net0 virtio,bridge=vmbr0,firewall=1 \
   --ide2 local-lvm:cloudinit \
   --serial0 socket --vga serial0 \
-  --agent 1  
+  --rng0 source=/dev/urandom \
+  --agent 1
   
 qm template 9001
 ```
-
-```bash
-qm clone 9001 101 --name test --full 1
-
-qm disk resize 101 scsi0 16G
-
-qm set 101 \
-  --ciupgrade 1 \
-  --ciuser dms --cipassword 'dms' \
-  --ipconfig0 ip=192.168.178.211/24,gw=192.168.178.1
-
-qm start 101
-```
-
 
 ```bash
 docker compose -f docker-compose.yml run --rm terraform init
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,19 @@
-
-
 resource "proxmox_vm_qemu" "ubuntu-test-instance" {
+  # ---------- Identity & source ----------
   vmid        = 1001
   name        = "ubuntu-test-instance"
   target_node = "pve"
   clone       = "ubuntu-24.04-cloud-template"
   full_clone  = true
-  agent       = 1
+
+  # ---------- Lifecycle (optional) ----------
+  # vm_state         = "running"
+  # automatic_reboot = true
+
+  # ---------- Hardware: compute & platform ----------
   memory      = 1024
+  balloon     = 1024
+  agent       = 1
   boot        = "order=scsi0"
   scsihw      = "virtio-scsi-single"
 
@@ -15,17 +21,7 @@ resource "proxmox_vm_qemu" "ubuntu-test-instance" {
     cores = 1
   }
 
-  ciupgrade  = true
-  nameserver = "192.168.178.1"
-  ipconfig0  = "ip=192.168.178.222/24,gw=192.168.178.1,ip6=dhcp"
-  skip_ipv6  = true
-  ciuser     = "dms"
-  cipassword = "dms"
-
-  serial {
-    id = 0
-  }
-
+  # ---------- Storage ----------
   disks {
     scsi {
       scsi0 {
@@ -44,9 +40,24 @@ resource "proxmox_vm_qemu" "ubuntu-test-instance" {
     }
   }
 
+  # ---------- Network ----------
   network {
-    id     = 0
-    model  = "virtio"
-    bridge = "vmbr0"
+    id       = 0
+    model    = "virtio"
+    bridge   = "vmbr0"
+    firewall = true
   }
+
+  # ---------- Console ----------
+  serial {
+    id = 0
+  }
+
+  # ---------- Cloud-Init ----------
+  ciupgrade  = true
+  nameserver = "192.168.178.1"
+  ipconfig0  = "ip=192.168.178.222/24,gw=192.168.178.1"
+  skip_ipv6  = true
+  ciuser     = "dms"
+  sshkeys    = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO8wQ3QpMwyZmfg1Hl97WByICUFVDKbT8yiyus7LWSW8 dsuprunov@gmail.com"
 }
